b61e1fa41090e3f4a5a5dc4a5ea872408402d189
F6 <dabsi@dabserver.com>
2024-10-15 17:01:50 -0500

    move initialization

--- src/tools/important/tankswars/TanksWars.kt
+++ src/tools/important/tankswars/TanksWars.kt
@@ -2,39 +2,11 @@ package tools.important.tankswars
 
 import main.Tanks
 import tanks.Game
-import tanks.Game.registerTank
 import tanks.extension.Extension
-import tanks.gui.screen.IConditionalOverlayScreen
-import tanks.gui.screen.ILevelPreviewScreen
-import tanks.gui.screen.Screen
-import tanks.gui.screen.ScreenGame
-import tanks.gui.screen.ScreenPartyLobby
+import tanks.gui.screen.*
 import tanks.gui.screen.leveleditor.ScreenLevelEditorOverlay
 import tanks.tank.Tank
-import tools.important.tankswars.building.BuildingType
 import tools.important.tankswars.core.*
-import tools.important.tankswars.event.to_client.*
-import tools.important.tankswars.tank.TankFiller
-import tools.important.tankswars.tank.TankSoldier
-import tools.important.tankswars.tank.TankSoldierCaptain
-import tools.important.tankswars.tank.TankSoldierDefender
-import tanks.network.NetworkEventMap.register as registerNetworkEvent
-
-
-private fun registerTank0W(tankClass: Class<out Tank>, name: String) = registerTank(tankClass, name, 0.0)
-
-
-private var fillerIndex = 0
-private fun registerFiller() {
-    fillerIndex++
-    registerTank0W(TankFiller::class.java, "tw_filler$fillerIndex")
-}
-
-private fun registerFiller(amount: Int) {
-    for (i in 1..amount) {
-        registerFiller()
-    }
-}
 
 object TanksWars {
     const val VERSION = "Tanks Wars 0.2.1"
@@ -46,24 +18,7 @@ var lastScreen: Screen? = null
 class TanksWarsExtension : Extension("TanksWars") {
     override fun setUp() {
         println("Currently running ${TanksWars.VERSION}")
-
-        registerNetworkEvent(EventNewsMessage::class.java)
-        registerNetworkEvent(EventBuildingWasCaptured::class.java)
-        registerNetworkEvent(EventBuildingWasSilentlyCaptured::class.java)
-        registerNetworkEvent(EventTeamFled::class.java)
-        registerNetworkEvent(EventTankDefeated::class.java)
-        registerNetworkEvent(EventBuildingWasDestroyed::class.java)
-
-        registerFiller(3) // skip to page 2
-
-        registerTank0W(TankSoldier::class.java, "tw_soldier")
-        registerTank0W(TankSoldierCaptain::class.java, "tw_soldiercaptain")
-        registerTank0W(TankSoldierDefender::class.java, "tw_soldierdefender")
-        registerFiller(10 - 3) // skip to next row
-
-        for (buildingType in BuildingType.entries) {
-            registerTank0W(buildingType.tankClass, buildingType.registryName)
-        }
+        initializeTanksWars()
     }
 
     override fun preUpdate() {
---
+++ src/tools/important/tankswars/core/Initialize.kt
@@ -0,0 +1,46 @@
+package tools.important.tankswars.core
+
+import tanks.Game.registerTank
+import tanks.tank.Tank
+import tools.important.tankswars.building.BuildingType
+import tools.important.tankswars.event.to_client.*
+import tools.important.tankswars.tank.TankFiller
+import tools.important.tankswars.tank.TankSoldier
+import tools.important.tankswars.tank.TankSoldierCaptain
+import tools.important.tankswars.tank.TankSoldierDefender
+import tanks.network.NetworkEventMap.register as registerNetworkEvent
+
+private fun registerTank0W(tankClass: Class<out Tank>, name: String) = registerTank(tankClass, name, 0.0)
+
+private var fillerIndex = 0
+private fun registerFiller() {
+    fillerIndex++
+    registerTank0W(TankFiller::class.java, "tw_filler$fillerIndex")
+}
+
+private fun registerFiller(amount: Int) {
+    for (i in 1..amount) {
+        registerFiller()
+    }
+}
+
+fun initializeTanksWars() {
+    registerNetworkEvent(EventNewsMessage::class.java)
+    registerNetworkEvent(EventBuildingWasCaptured::class.java)
+    registerNetworkEvent(EventBuildingWasSilentlyCaptured::class.java)
+    registerNetworkEvent(EventTeamFled::class.java)
+    registerNetworkEvent(EventTankDefeated::class.java)
+    registerNetworkEvent(EventBuildingWasDestroyed::class.java)
+
+    registerFiller(3) // skip to page 2
+
+    registerTank0W(TankSoldier::class.java, "tw_soldier")
+    registerTank0W(TankSoldierCaptain::class.java, "tw_soldiercaptain")
+    registerTank0W(TankSoldierDefender::class.java, "tw_soldierdefender")
+    registerFiller(10 - 3) // skip to next row
+
+    for (buildingType in BuildingType.entries) {
+        registerTank0W(buildingType.tankClass, buildingType.registryName)
+    }
+    registerFiller(BuildingType.entries.size)
+}
