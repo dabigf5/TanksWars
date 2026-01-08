40f6fdb8263c1bf4fa33e8fecbc2f59f97623b27
F6 <decrepit_danny@important.tools>
2026-01-08 03:02:54 -0600

    refactor this thing
    Signed-off-by: F6 <decrepit_danny@important.tools>

--- src/tools/important/tankswars/util/TeamsAllied.kt
+++ src/tools/important/tankswars/util/TeamsAllied.kt
@@ -3,7 +3,4 @@ package tools.important.tankswars.util
 import tanks.Team
 
 // this function should really be a method in Team in vanilla
-fun teamsAllied(a: Team?, b: Team?): Boolean {
-    if (a == null || b == null) return false
-    return a == b
-}
+fun teamsAllied(a: Team?, b: Team?): Boolean = a != null && b != null && a == b
